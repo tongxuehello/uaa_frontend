6fd471a99523992bd3dc7d4ea026412fd75e9c89
邵慧萌 <shaohuimeng@qiyi.com>
2016-06-23 18:17:27 +0800

    项目正常运行。增加autoprefixer插件，解决scss加载出错的bug。

--- webpack.config.js
+++ webpack.config.js
@@ -30,18 +30,23 @@ module.exports = {
 			},
 			{
 				test : /\.less$/,
-				include : [path.join(__dirname, 'css')],
-				loader : 'style-loader!css-loader!less-loader'
+				include : [path.join(__dirname, 'src')],
+				exclude : /node_modules/,
+				loader : 'style!css!less'
 			}, 
 			{
 				test : /\.scss$/,
-				loader : 'style-loader!css-loader!sass-loader'
+				include : [path.join(__dirname, 'src')],
+				exclude : /node_modules/,
+				loader : 'style!css?modules&importLoaders=2&sourceMap&localIdentName=[local]___[hash:base64:5]!autoprefixer?browsers=last 2 version!sass?outputStyle=expanded&sourceMap'
 			}, {
 				test : /\.(css)$/,
-				include : [path.join(__dirname, 'css')],
-				loader : 'style-loader!css-loader'
+				include : [path.join(__dirname, 'src')],
+				exclude : /node_modules/,
+				loader : 'style!css'
 			}, {
 				test : /\.(png|jpg)$/,
+				exclude : /node_modules/,
 				loader : 'url-loader?limit=8192'
 			}
 		]
