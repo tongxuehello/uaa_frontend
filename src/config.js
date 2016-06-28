88128b51892a30d378817898fe96350d42fa3c4a
邵慧萌 <shaohuimeng@qiyi.com>
2016-06-28 18:38:42 +0800

    [TBD]

--- src/config.js
+++ src/config.js
@@ -15,21 +15,19 @@ module.exports = Object.assign({
   apiHost: process.env.APIHOST || 'localhost',
   apiPort: process.env.APIPORT,
   app: {
-    title: 'React Redux Example',
-    description: 'All the modern best practices in one example.',
+    title: 'UAA Frontend',
+    description: 'uaa前端项目',
     head: {
-      titleTemplate: 'React Redux Example: %s',
+      titleTemplate: 'UAA Frontend: %s',
       meta: [
         {name: 'description', content: 'All the modern best practices in one example.'},
         {charset: 'utf-8'},
-        {property: 'og:site_name', content: 'React Redux Example'},
+        {property: 'og:site_name', content: 'UAA Frontend'},
         {property: 'og:image', content: 'https://react-redux.herokuapp.com/logo.jpg'},
         {property: 'og:locale', content: 'en_US'},
-        {property: 'og:title', content: 'React Redux Example'},
+        {property: 'og:title', content: 'UAA Frontend'},
         {property: 'og:description', content: 'All the modern best practices in one example.'},
-        {property: 'og:card', content: 'summary'},
-        {property: 'og:site', content: '@erikras'},
-        {property: 'og:creator', content: '@erikras'},
+        {property: 'og:creator', content: '@huimengshao@qq.com'},
         {property: 'og:image:width', content: '200'},
         {property: 'og:image:height', content: '200'}
       ]
--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -11,22 +11,7 @@ import { isLoaded as isAuthLoaded, load as loadAuth, logout } from '../../redux/
 import { InfoBar } from '../../components';
 import { push } from 'react-router-redux';
 import config from '../../config';
-import { asyncConnect } from 'redux-async-connect';
 
-@asyncConnect([{
-  promise: ({store: {dispatch, getState}}) => {
-    const promises = [];
-
-    if (!isInfoLoaded(getState())) {
-      promises.push(dispatch(loadInfo()));
-    }
-    if (!isAuthLoaded(getState())) {
-      promises.push(dispatch(loadAuth()));
-    }
-
-    return Promise.all(promises);
-  }
-}])
 @connect(
   state => ({user: state.auth.user}),
   {logout, pushState: push})
